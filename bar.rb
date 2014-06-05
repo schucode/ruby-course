e152870366d039874ca6c70267e3ee7e8d1d8ae7
vagrant <vagrant@student-dev-box>
2014-06-05 11:19:26 -0500

    Add working  add_menu_item method

--- bar.rb
+++ bar.rb
@@ -1,4 +1,16 @@
 require 'time' # you're gonna need it
 
 class Bar
+	attr_reader :name
+	attr_accessor :menu_items
+
+	def initialize(name, menu_items={})
+		@name = name
+		@menu_items = menu_items
+	end
+
+	def add_menu_item(item, price)
+		@menu_items[item] = price
+	end
+
 end
