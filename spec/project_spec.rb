8bfd4a6c87dc0b96643edcb10a3a33573dded781
Mark Schumaker <m.schumaker235@gmail.com>
2014-06-10 10:56:41 -0500

    All Task and nearly  Project tests complete

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,17 +1,34 @@
 require 'spec_helper'
 
 describe 'Project' do
+
+	before do
+    @project = TM::Project.new("project one")
+  end
+
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
 
-  xit "must have a name" do
-  	project = TM::Project.new("whatever")
-  	expect(project.name).to eq("whatever")
+  it "must have a name" do
+  	expect(project.name).to eq("project one")
   end
 
-  xit "must have an id" do
-  	project = TM::Project.new("whatever")
-  	expect()
+  it "must have an id" do
+  	i = @task.stub(:id).and_return(34)
+  	expect(task.id).to eq(34)
+  end
+
+  it "should be able to set tasks as completed by id" do
+  	#expect(@task).to receive(:complete, 3).and_return(false)
+  	task = TM::Task.new("somename", "do stuff", 3)
+  	task.stub(:id).and_return(23)
+  	@project.set_complete(23)
+  	expect(task.complete).to eq(true)
   end
 end
+
+
+# A project can retrieve a list of all complete tasks, sorted by creation date
+# A project can retrieve a list of all incomplete tasks, sorted by priority
+# If two tasks have the same priority number, the older task gets priority
